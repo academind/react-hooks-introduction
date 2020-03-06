bba5cfe07724db7573ccaea2c59cd8ca9c3032ee
Don-Stevenson <dsteven1@gmail.com>
2020-03-06 18:38:44 -0500

    now using useState and hooks instead of class

--- src/App.js
+++ src/App.js
@@ -3,44 +3,41 @@ import React, { useState } from "react";
 import CharPicker from "./components/CharPicker";
 import Character from "./components/Character";
 
-
-
 const App = props => {
-  const [state, setState] = useState({
-  });
-  const [destroyed, setdestroyed] = useState(false)
-  const [chosenSide, setChosenSide] = useState("light")
-  const [selectedCharacter, setSelectedCharacter] = useState(1)
+  const [state, setState] = useState({});
+  const [destroyed, setDestroyed] = useState(false);
+  const [chosenSide, setChosenSide] = useState("light");
+  const [selectedCharacter, setSelectedCharacter] = useState(1);
   const sideHandler = side => {
-    setState({ ...state, side: side });
+    setChosenSide({ side });
   };
 
   const charSelectHandler = event => {
     const charId = event.target.value;
-    setState({  ...state, selectedCharacter: charId });
+    setSelectedCharacter({ charId });
   };
 
   const destructionHandler = () => {
-    setState({  ...state, destroyed: true });
+    setDestroyed(true);
   };
 
   let content = (
     <React.Fragment>
       <CharPicker
-        side={state.side}
-        selectedChar={state.selectedCharacter}
+        side={chosenSide}
+        selectedChar={selectedCharacter}
         onCharSelect={charSelectHandler}
       />
-      <Character selectedChar={state.selectedCharacter} />
+      <Character selectedChar={selectedCharacter} />
       <button onClick={sideHandler.bind(this, "light")}>Light Side</button>
       <button onClick={sideHandler.bind(this, "dark")}>Dark Side</button>
-      {state.side === "dark" && (
+      {chosenSide === "dark" && (
         <button onClick={destructionHandler}>DESTROY!</button>
       )}
     </React.Fragment>
   );
 
-  if (state.destroyed) {
+  if (destroyed) {
     content = <h1>Total destruction!</h1>;
   }
   return content;
